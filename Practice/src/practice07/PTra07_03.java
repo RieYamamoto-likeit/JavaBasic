8b353108b1a54d073a374da48f8b9672e7f9181b
RieYamamoto <yama.ri95.02@gmail.com>
2018-05-14 18:21:09 +0900

    PTra07_05まで完了

--- Practice/src/practice07/PTra07_03.java
+++ Practice/src/practice07/PTra07_03.java
@@ -10,9 +10,31 @@ public class PTra07_03 {
 	public static void main(String[] args) {
 
 		// ★ char型の配列keywordを定義し 「'平', '島', '沼', '山', '森'」 で初期化してください
+		char[] keyword = new char[] {'平', '島', '沼', '山', '森'};
+
+
+
+//		char keyword;
+//		keyword = '平';
+//		keyword = '島';
+//		keyword = '沼';
+//		keyword = '山';
+//		keyword = '森';
 
 
 		// ★ 配列keywordに格納されている値を後ろから順番に全て表示してください
+		for(int i = keyword.length -1;i>=0;i--) {
+			System.out.println(keyword[i]);
+		}
+//		(keyword.length-1)←どれだけ増えても大丈夫
+//
+
+
+
+//		int char;
+//		for(char keyword=0; i<keyword -1 ;i--) {
+//			System.out.println(char[i]);
+//		}
 
 	}
 }
--- Practice/src/practice07/PTra07_05.java
+++ Practice/src/practice07/PTra07_05.java
@@ -10,7 +10,8 @@ public class PTra07_05 {
 	public static void main(String[] args) {
 
 		// ★ int型の配列arrayを宣言して、5個の領域を確保してください
-
+		int[] array;
+		 array=new int[5];
 
 		/*
 		 * ★ 下記の仕様でfor文を記述してください
@@ -23,10 +24,19 @@ public class PTra07_05 {
 
 			// ★ 0～100000の中からランダムで値を取得して、配列arrayのi番目に代入してください
 
+//		 for( int i=0 ; array>i ; i++) {
+		 for (int i = 0; i < array.length; i++) {
+			  array[i] = new java.util.Random().nextInt(100001);
+
+
+
+
+
+//		 int array[i] = new java.util.Random().nextInt(100001);
 
 			// ★ 配列arrayのi番目の値を出力してください
-
-
+			  System.out.println(array[i]);
+		 }
 		//------for end
 	}
 }
