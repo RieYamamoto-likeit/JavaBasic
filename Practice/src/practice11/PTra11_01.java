07aeacc657a918268ae5c74369be4d86bd4fcf62
RieYamamoto <yama.ri95.02@gmail.com>
2018-06-07 19:00:39 +0900

    PTra11_1まで完了

--- Practice/src/practice11/PTra11_01.java
+++ Practice/src/practice11/PTra11_01.java
@@ -13,14 +13,38 @@ public class PTra11_01 {
 		 * 以下のプログラムではコンパイルエラーが起きてしまいます
 		 * ★ コンパイルが通るように修正してください(引数5つのコンストラクタを利用すること)
 		 */
-		Book book = new Book();
-		book.title = "スッキリわかるJava入門";
-		book.author = "中山清喬／国本大悟";
-		book.price = 2600;
-		book.pageSize = 657;
-		book.publication = "インプレス";
+
+		Book book = new Book("スッキリわかるJava入門","中山清喬／国本大悟",2600,657,"インプレス");
+//		book.title = "スッキリわかるJava入門";
+//		book.author = "中山清喬／国本大悟";
+//		book.price = 2600;
+//		book.pageSize = 657;
+//		book.publication = "インプレス";
 
 		// 本の情報を出力します
 		System.out.println(book.dispBookInfo());
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//		Book book1 = new Book("スッキリわかるJava入門");
+//		Book book2 = new Book("中山清喬／国本大悟");
+//		Book book3 = new Book(2600);
+//		Book book4 = new Book(657);
+//		Book book5 = new Book("インプレス");
