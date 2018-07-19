bb259093db5fd033d65c68b094ec189eec668064
RieYamamoto <yama.ri95.02@gmail.com>
2018-07-19 18:52:52 +0900

    PTra13-7まで完了

--- Practice/src/practice13/ptra13/PTra13_07.java
+++ Practice/src/practice13/ptra13/PTra13_07.java
@@ -6,6 +6,11 @@
  */
 package practice13.ptra13;
 
+//import practice13.common.Hero;
+import practice13.common.Item;
+import practice13.common.Slime;
+import practice13.common.SuperHero;
+
 public class PTra13_07 {
 
 	/*
@@ -15,23 +20,46 @@ public class PTra13_07 {
 	public static void main(String[] args) {
 
 		// ★ SuperHeroインスタンスとSlimeインスタンスを作成し、それぞれの名前に"勇者（装備あり）", "スライム"を設定してください
-
+		SuperHero sh=new SuperHero();
 
 		// ★ Itemクラスのインスタンスを作成し、("こんぼう", 4）をコンストラクタの引数にしてください
-
+		Item item=new Item();
+		item.setName("こんぼう");
+		item.setAdditionalDamage(4);
 
 		// ★ 作成したItemインスタンスをSuperHeroに持たせてください
+		sh.setEquipment(item);
 
 
+
+		Slime slime=new Slime();
+
+		sh.setName("勇者");
+		slime.setName("スライム");
 		/*
 		 * ★ SuperHeroとSlimeを、どちらかが体力０になるまで戦わせます
 		 *
 		 * 	●SuperHeroの攻撃 -> ダメージ判定 -> Slimeの攻撃 -> ダメージ判定
 		 * 上記を繰り返し行います
 		 */
+			while(true) {
+
+				boolean shs = slime.damage(sh.attack());
+
+				if(shs) {
+					System.out.println("「"+sh.getName()+"は"+slime.getName()+"との戦闘に勝利した」");
+					break;
+				}
+
+				boolean hh = sh.damage(slime.attack());
+				if(hh) {
+					System.out.println("「"+slime.getName()+"は"+sh.getName()+"との戦闘に勝利した」");
+					break;
+				}
+			}
 
 
 		// ★ 勝利した方の出力を行ってください。「○○は■■との戦闘に勝利した」
-
+		}
 	}
-}
+
