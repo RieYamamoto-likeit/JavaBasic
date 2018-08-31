08d0e957a3d84ba3d5316f7571d0814f4e1a9786
RieYamamoto <yama.ri95.02@gmail.com>
2018-08-31 18:57:43 +0900

    PTra17－4まで完了しました。

--- Practice/src/practice17/ptra17/PTra17_04.java
+++ Practice/src/practice17/ptra17/PTra17_04.java
@@ -15,6 +15,21 @@ public class PTra17_04 {
 		 * ★ strに入っている文字列を1文字ずつゆっくり表示させるプログラムを完成させてください
 		 * ※ Thread.sleep を使用すること
 		 */
+		String abc = "スッキリわかるjava入門";
 
+		char data[] = abc.toCharArray();
+
+		int arr_num = data.length;
+
+		for(int i = 0;i <= arr_num-1; i++) {
+
+		try {
+		Thread.sleep(1000);
+		}
+		catch(InterruptedException e) {
+			}
+		System.out.print(data[i]);
+
+		}
 	}
 }
