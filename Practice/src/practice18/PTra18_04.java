f667df7e550fdddb6c77002b5c344c1954207737
RieYamamoto <yama.ri95.02@gmail.com>
2018-09-10 15:34:15 +0900

    PTra18-4まで完了

--- Practice/src/practice18/PTra18_04.java
+++ Practice/src/practice18/PTra18_04.java
@@ -6,6 +6,14 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+import practice18.entity.Player;
+
 public class PTra18_04 {
 
 	/*
@@ -19,9 +27,78 @@ public class PTra18_04 {
 		 * ★ file/BestElevenCandidate.csvの内容を取得し、１行毎にPlayerインスタンスに情報を格納してください
 		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
 		 */
+		 ArrayList<Player> array = new ArrayList<>();
+	        try(Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))) {
+	            while (scanner.hasNext()) {
+	                String line = scanner.nextLine();
+
+	                String[] list = line.split(",",0);
+	                Player player = new Player();
+
+	            	player.setPosition(list[0]);
+             	player.setName(list[1]);
+             	player.setCountry(list[2]);
+             	player.setTeam(list[3]);
+
+             	  array.add(player);
+
+
+
+
+	            }
+	        } catch (FileNotFoundException e) {
+	            System.out.println("ファイルが見つかりません");
+	        }
+
 
 
 		// ★ ①のArrayListの中からGK1名、DF4名、MF4名, FW2名をランダムで出力してください
+
+
+	        ArrayList<Player> GK = new ArrayList<>();
+
+	        ArrayList<Player> DF = new ArrayList<>();
+
+	        ArrayList<Player> MF = new ArrayList<>();
+
+	        ArrayList<Player> FW = new ArrayList<>();
+
+	        for(Player pl:array) {
+	        	if(pl.getPosition().equals("GK")) {
+	        		GK.add(pl);
+	        	}
+	        	if(pl.getPosition().equals("DF")) {
+	        		DF.add(pl);
+	        	}
+	        	if(pl.getPosition().equals("MF")) {
+	        		MF.add(pl);
+	        	}
+	        	if(pl.getPosition().equals("FW")) {
+	        		FW.add(pl);
+	        	}
+	        }
+
+	        Collections.shuffle(GK);
+	        Collections.shuffle(DF);
+	        Collections.shuffle(MF);
+	        Collections.shuffle(FW);
+
+	        for(int i = 0;i < 1 ; i++ ) {
+	        	System.out.println(GK);
+	        }
+	        for(int i = 0;i < 4 ; i++) {
+	        	System.out.println(DF);
+	        }
+	        for(int i = 0;i < 4 ; i++) {
+	        	System.out.println(MF);
+	        }
+	        for(int i = 0;i < 2 ; i++) {
+	        	System.out.println(FW);
+	        }
+
+
+
+
 
 	}
 }
